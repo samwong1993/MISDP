4ba6f3376a4ce62fa2340a35b6f028f3b3f36125
HUANGSEN <huangsen1993@gmail.com>
2020-09-01 18:18:12 +0800

    UPDATE

---
+++ solve_x.m
@@ -0,0 +1,19 @@
+function param = solve_x(param)
+    obj = objective(param);
+    rho = param.rho;
+    x_0 = param.x_0;
+    for i = 1:100000
+        x = param.x;
+        obj_old = obj;
+        g_x = grad_x(param);
+        x = x - 0.01*g_x;
+        if rho >= 0 & norm(x - x_0)>rho
+            x = (x - x_0)/norm(x - x_0)*rho + x_0;
+        end
+        param.x = x;
+        obj = objective(param);
+        if abs(obj-obj_old)<1e-7
+            break
+        end
+    end
+end
