125343a3de8162e348e46a85ba384bba8b3862b7
HUANGSEN <huangsen1993@gmail.com>
2020-09-02 19:30:03 +0800

    UPDATE

--- main.m
+++ main.m
@@ -1,15 +1,14 @@
 clear all
 load demo_3_D
-% lambda = 3.3;
-% M = 7;
-% d = 2;
-% param = generator(lambda,M,d);
-% param.a = param.a + 2*randn(1,M);
+lambda = 3.3;
+M = 6;
+d = 3;
+param = generator(lambda,M,d);
+%param.a = param.a + 2*randn(1,M);
 H = [];
 Y = [];
 z_Upper = 9999;
 z_Lower = -9999;
-i = 1;
 range_n = range(param);
 param.n = [0,0,0]
 param.x = [0,0]
@@ -21,6 +20,8 @@ for i = 1:M-1
 end
 i = i + 1;
 str = str + "%2.0f";
+k = 1;
+i = 1;
 while(abs(z_Upper - z_Lower) > 1e-7)
     cvx_solver mosek
     [z,X,G,n] = OA_master(param,H,Y);
@@ -28,17 +29,19 @@ while(abs(z_Upper - z_Lower) > 1e-7)
     cvx_solver SDPT3
     param.n = n';
     [G_p,X_p,S_p] = NLP_P(param);
-	[H_d,Y_d,S_d] = NLP_D(param);
+    [H_d,Y_d,S_d] = NLP_D(param);
     if S_p < z_Upper
+        k = 1;
         z_Upper = S_p;
         n_best = n;
     end
     H(:,:,i) = H_d;
     Y(:,:,i) = Y_d;
     i = i + 1;
+    k = k + 1;
     fprintf("LB:("+str+")|UP:("+str+")\n",range_n(:,1),range_n(:,2))
     fprintf("n_e:("+str+")|n:("+str+")|Gap:%2.2f\n",param.n_e,n',abs(z_Upper - z_Lower))
-    if i > 20 & z_Upper<10 | i > 50
+    if k > 20 & z_Upper<2 | i > 50
         break
     end
 end
